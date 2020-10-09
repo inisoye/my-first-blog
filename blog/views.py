61450b97632175cb3bdf23252d95b68bd1ed9c12
Inioluwa Akinyosoye <inisoye@gmail.com>
2020-10-09 04:17:05 +0100

    Change HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Views request information from the model you created before and pass it to a template
+
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
